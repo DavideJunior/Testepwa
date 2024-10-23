60be83e3ae407243802a8a9d496267e1a4e14c9c
DavideJunior <davide.junior@alu.unibalsas.edu.br>
2024-10-22 21:08:36 -0300

    delete

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './containers/Home';
 import AdvancedSearch from './containers/AdvancedSearch';
 import Map from './containers/Map';
-=======
-import Home from './containers/Home'; // A importação deve funcionar se index.jsx tiver um export default
->>>>>>> 5e4cacfb0849da152b62efb59019591e78b0ea4c
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-<<<<<<< HEAD
     <Router>
       <Routes>
       <Route path="/Home" element={<Home />} /> 
@@ -24,8 +19,5 @@ root.render(
         <Route path="/Map" element={<Map />} />           {/* Página do mapa de incidentes */}
       </Routes>
     </Router>
-=======
-    <Home />
->>>>>>> 5e4cacfb0849da152b62efb59019591e78b0ea4c
   </StrictMode>
 );
